271ae14a3505cec28ed0fdb276cde4765f422f50
Maisa Ahmad <2013maisaah@gmail.com>
2021-05-21 16:01:14 -0400

    fixed navigation

--- src/Pages/App.js
+++ src/Pages/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import {Switch, Route, Router} from '../util/router.js';
+import {Switch, Route} from '../util/router.js';
+import { BrowserRouter as Router} from 'react-router-dom';
 import Home from './Home';
 import About from './About';
+import Navigation from "../Components/Plot/React/Navigation";
 
 function App() {  
   return(
