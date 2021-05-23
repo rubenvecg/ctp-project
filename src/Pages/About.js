7ba5f727c7469b54555b6080b400f60edf77974b
Maisa Ahmad <2013maisaah@gmail.com>
2021-05-23 12:51:06 -0400

    replaced css file

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import '../App.scss';
 import Navigation from "../Components/Plot/React/Navigation";
 import Section from "../Components/Plot/React/Section";
 
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../App.css';
+import '../App.scss';
 import Dashboard from "../Sections/Dashboard/Dashboard"
 import "../Styles/ChartStyles.css"
 import Navigation from "../Components/Plot/React/Navigation";
