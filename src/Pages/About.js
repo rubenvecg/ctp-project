fdabe6f341ad7ecfeeb215c1d1153bb04bda193b
Maisa Ahmad <2013maisaah@gmail.com>
2021-05-25 10:34:17 -0400

    removed component

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -49,17 +49,6 @@ function About() {
         Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         />
-        <AboutCard 
-        img={placeholder} 
-        github="https://github.com/Maisa-ah"
-        linkedin="https://www.linkedin.com/in/maisa-ahmad/"
-        name="Maisa Ahmad" 
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        />
 
       </div>
   );
