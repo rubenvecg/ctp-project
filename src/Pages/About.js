6c795a4a813deb4cfbd965ad9d9bcce7082a2e14
Maisa Ahmad <2013maisaah@gmail.com>
2021-05-25 17:50:51 -0400

    added text

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -13,41 +13,28 @@ function About() {
         <Navigation/>
         <Section 
         title="About us" 
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. "
+        text="NYC Crime View is a visualization app that enables you to view crime data 
+        retrieved from the New York City Police Department between the years 2006 and 2020 
+        across different city jurisdictions. We worked with a dataset with over 5 million 
+        entries containing the location of the crime, type of crime and perpetrator descriptions."
         />
-        <Section 
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        <Section className="Section-disclaimer"
+        text="The objective for this project is not to provide any insights related to the city’s 
+        crime activity since the data comes directly from NYPD. Any biases or inconsistencies are 
+        related directly with the creation of this dataset and NYPD’s operations."
         />
         <Source
         s1="https://www1.nyc.gov/site/planning/data-maps/open-data/districts-download-metadata.page"
         s1Title="Political and Administrative Districts, Department of City Planning; retrieved from https://www1.nyc.gov/."
-        s2=""
-        s2Title=""
-        s3=""
-        s3Title=""
+        s2="https://data.cityofnewyork.us/Public-Safety/NYPD-Arrest-Data-Year-to-Date-/uip8-fykc"
+        s2Title="NYC OpenData, Police Department (NYPD); retrieved from https://data.cityofnewyork.us/."
         />
         <AboutCard 
         img={placeholder} 
         github="https://github.com/rubenvecg"
         linkedin="https://www.linkedin.com/in/ruben-vecino/"
         name="Ruben Vecino" 
-        text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        text="Originally from Barranquilla, Colombia. I am an undergraduate Computer Systems Technology student at the New York City College of Technology (City Tech). My professional interests include Software Engineering, Data Analytics & Visualization, and Full Stack Web Development. I am currently looking for internships and/or work opportunities."
         />
 
       </div>
