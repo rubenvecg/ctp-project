ce58bae27212570ad710de0c3724b2d3c754d9ec
Maisa Ahmad <2013maisaah@gmail.com>
2021-05-26 00:44:58 -0400

    added factsheet

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import '../App.scss';
-import Dashboard from "../Sections/Dashboard/Dashboard"
-import "../Styles/ChartStyles.css"
+import Dashboard from "../Sections/Dashboard/Dashboard";
+import "../Styles/ChartStyles.css";
 import Navigation from "../Components/Plot/React/Navigation";
+import FactSheet from "../Components/Plot/React/FactSheet";
 
 
 function Home() {  
@@ -11,6 +12,7 @@ function Home() {
       <div>
         <Navigation/>
         <Dashboard></Dashboard>
+        <FactSheet/>
       </div>
   );
 }
